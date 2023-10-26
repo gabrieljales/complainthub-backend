b5cf5d9c58b4300bdbe530464229632bec3d220b
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-25 22:16:28 -0300

    feat: create Complaint entity

---
+++ src/modules/complaints/infra/typeorm/entities/Complaint.ts
@@ -0,0 +1,36 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "../../../../accounts/infra/typeorm/entities/User";
+
+export enum ComplaintStatusEnum {
+  SOLVED = 'solved',
+  UNDER_ANALYSIS = 'underAnalysis',
+  UNSOLVED = 'unsolved',
+}
+
+@Entity("complaints")
+export class Complaint {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: ComplaintStatusEnum,
+    default: ComplaintStatusEnum.UNDER_ANALYSIS,
+  })
+  status: ComplaintStatusEnum;
+
+  @ManyToOne(() => User, (user) => user.complaints)
+  @JoinColumn({
+    name: 'user_id',
+  })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
