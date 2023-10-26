9d7bb1a8ac1ebe0fc4bf75993050b59df68e2761
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-26 19:35:02 -0300

    feat: create UsersController create and list methods

--- src/modules/accounts/controllers/UsersController.ts
+++ src/modules/accounts/controllers/UsersController.ts
@@ -1,9 +1,31 @@
 import { Request, Response } from "express";
+import { UsersService } from "../services/UsersService";
 
 class UsersController {
   async create(request: Request, response: Response): Promise<Response> {
+    // Extraindo dados do corpo da requisição
+    const { email, last_name, name, password, type } = request.body;
+    const usersService = new UsersService();
 
-    return response.json("Esse método cria um usuário");
+    // Utilizando UsersService para salvar o usuário
+    await usersService.create({
+      email,
+      last_name,
+      name,
+      password,
+      type
+    });
+
+    // Retornando código 201 (created)
+    return response.status(201).send();
+  }
+
+  async list(request: Request, response: Response): Promise<Response> {
+    const usersService = new UsersService();
+
+    const users = await usersService.list();
+
+    return response.json(users);
   }
 }
 
