b10276968e5675cbee6a164b7f9ad6bb1385f9e2
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-27 21:19:09 -0300

    feat: create authenticate route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,15 @@
 import { Router } from "express";
 import { usersRoutes } from "./UsersRoutes";
 import { complaintsRoutes } from "./ComplaintsRoutes";
+import { authenticateRoutes } from "./AuthenticateRoutes";
 
 const router = Router(); // Instanciando um Router
 
 // Rotas de usuários
 router.use("/users", usersRoutes);
+
+// Rota de autenticação
+router.use(authenticateRoutes); // Sem path (/)
 
 // Rotas de reclamações
 router.use("/complaints", complaintsRoutes);
