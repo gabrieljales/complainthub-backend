110bcf5471a86d47d62cecf27a843e9038680d3c
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-28 14:13:52 -0300

    feat: add security improvements on findById, delete and update methods

--- src/modules/complaints/services/ComplaintsService.ts
+++ src/modules/complaints/services/ComplaintsService.ts
@@ -39,43 +39,56 @@ export class ComplaintsService {
   }
 
   // Método responsável por buscar uma reclamação por id
-  async findById(id: number): Promise<Complaint> {
-    const complaint = await ComplaintsRepository.findOne({ where: { id } });
+  // O usuário deve ter criado a reclamação ou ser um manager, para ver uma reclamação que não foi ele que criou
+  async findById(id: number, user_id: number, user_type: UserTypeEnum): Promise<Complaint> {
+    const complaint = await ComplaintsRepository.findOne({ where: { id }, relations: ["user"] });
+  
+    // Se a reclamação não existir ou o usuário autenticado não for o autor da reclamação e não for um gerente, lançamos um erro
+    if (!complaint || (complaint.user.id !== user_id && user_type !== UserTypeEnum.MANAGER)) {
 
+      // Dica de segurança: retornar um erro “Not Found” (404) em vez de “Unauthorized” (403) pode ser uma boa prática de segurança.
+      // Isso pode evitar a divulgação de informações indesejadas sobre a existência de recursos que o usuário não tem permissão para acessar
+      throw new AppError("Complaint not found", 404);
+    }
+  
     return complaint;
   }
-
   // Método responsável por deletar uma reclamação de acordo com o id
-  async delete(id: number): Promise<void> {
+  async delete(id: number, user_id: number, type: string): Promise<void> {
     // Primeiro, verificamos se a reclamação realmente existe
-    const complaint = await this.findById(id);
+    let complaint = await ComplaintsRepository.findOne({ where: { id } });
 
-    // Se a reclamação não existir, devemos lançar um erro BadRequest
-    if (!complaint) {
+    // Se a reclamação não existir ou o usuário autenticado não for o autor da reclamação e não for um gerente, lançamos um erro
+    // Retornamos 404 invés de 403 pelo mesmo motivo explicado no método anterior, findById
+    if (!complaint || (type === 'client' && complaint.user.id !== user_id)) {
       throw new AppError("Complaint not found", 404);
     }
 
     // Deletando a reclamação
-    await ComplaintsRepository.delete(complaint);
+    await ComplaintsRepository.delete(complaint.id);
   }
 
   // Método responsável por atualizar uma reclamação de acordo com o id
-  async update(id: number, data: IUpdateComplaintDTO, user_type: UserTypeEnum): Promise<Complaint> {
-    let complaint = await this.findById(id);
-
+  async update(
+    id: number,
+    data: IUpdateComplaintDTO,
+    user_type: UserTypeEnum
+  ): Promise<Complaint> {
+    let complaint = await ComplaintsRepository.findOne({ where: { id }, relations: ["user"] });
+  
     // Se a reclamação não existir, devemos lançar um erro BadRequest
     if (!complaint) {
       throw new AppError("Complaint not found", 404);
     }
-
+  
     // Se o status estiver sendo atualizado, verifique se o usuário é do tipo manager
     if (data.status && user_type !== UserTypeEnum.MANAGER) {
       throw new AppError("Only managers can update the status", 403);
     }
-
+  
     // Combinando a reclamação existente com os novos dados
     complaint = ComplaintsRepository.merge(complaint, data);
-
+  
     // Retornando a reclamação atualizada no banco de dados
     return await ComplaintsRepository.save(complaint);
   }
