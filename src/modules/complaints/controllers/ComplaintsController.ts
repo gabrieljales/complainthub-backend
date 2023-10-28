176fe08a32ad0df921ff5003b494f6501725d318
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-28 14:15:10 -0300

    feat: create update complaints method and add improvements on findById and delete

--- src/modules/complaints/controllers/ComplaintsController.ts
+++ src/modules/complaints/controllers/ComplaintsController.ts
@@ -12,7 +12,7 @@ export class ComplaintsController {
     await complaintsService.create({
       description,
       title,
-      user_id: id // TODO: Depois pegar o user_id do usuário logado atualmente, via const { id } = request.user;
+      user_id: id
     });
 
     // Retornando código 201 (created)
@@ -20,7 +20,6 @@ export class ComplaintsController {
   }
 
   async list(request: Request, response: Response): Promise<Response> {
-    // TODO: Só o admin poderá ver essa rota
     const complaintsService = new ComplaintsService();
 
     const complaints = await complaintsService.list();
@@ -29,42 +28,38 @@ export class ComplaintsController {
   }
 
   async findById(request: Request, response: Response): Promise<Response> {
-    // Id obtido dos parâmetros da rota
+    // Id da reclamação obtido dos parâmetros da rota
     const { id } = request.params;
+    const { id: user_id, type } = request.user; // Obtendo tipo do usuário autenticado/logado
 
     const complaintsService = new ComplaintsService();
 
-    const complaint = await complaintsService.findById(+id); // Convertendo id para número
-
-    // Se a reclamação não existir, devemos lançar um erro BadRequest
-    if (!complaint) {
-      throw new AppError("Complaint not found", 404);
-    }
+    const complaint = await complaintsService.findById(+id, user_id, type); // +id: Convertendo id de string para número
 
     return response.json(complaint);
   }
 
   async delete(request: Request, response: Response): Promise<Response> {
-    // Id obtido dos parâmetros da rota
+    // Id da reclamação obtido dos parâmetros da rota
     const { id } = request.params;
+    const { id: user_id, type } = request.user; // Pegando id e tipo do usuário
 
     const complaintsService = new ComplaintsService();
 
-    // Busca a reclamação no banco de dados
-    const complaint = await complaintsService.findById(+id);
-
-    // Se a reclamação não existir, retorna um erro
-    if (!complaint) {
-      throw new AppError("Complaint not found", 404);
-    }
-
-    // Passando o ir obtido dos parâmetros da rota (o operador '+' serve para converter para número)
-    await complaintsService.delete(+id);
+    await complaintsService.delete(+id, user_id, type);
 
     return response.status(204).send();
   }
 
-  async update() {
-    // TODO: Pegar user_type do usuário logado
+  async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params; // Id da reclamação obtido dos parâmetros da rota
+    const data = request.body;
+    const { type } = request.user; // Obtendo tipo do usuário autenticado/logado
+  
+    const complaintsService = new ComplaintsService();
+  
+    await complaintsService.update(+id, data, type);
+  
+    return response.status(200).send();
   }
 }
