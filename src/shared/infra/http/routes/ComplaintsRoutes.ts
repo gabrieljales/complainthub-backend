cd3b514a76e519cdfc2715702768b7f83c49a91a
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-27 21:17:48 -0300

    feat: add list, findById and delete routes

--- src/shared/infra/http/routes/ComplaintsRoutes.ts
+++ src/shared/infra/http/routes/ComplaintsRoutes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { ComplaintsController } from "../../../../modules/complaints/controllers/ComplaintsController";
+import { EnsureAuthenticated } from "../middlewares/EnsureAuthenticated";
 
 const complaintsRoutes = Router(); // Instanciando um Router
 
 const complaintsController = new ComplaintsController(); // Instanciando um ComplaintsController
 
-complaintsRoutes.post("/", complaintsController.create);
+complaintsRoutes.post(
+  "/",
+  // EnsureAuthenticated,
+  complaintsController.create
+);
+complaintsRoutes.get("/", complaintsController.list); // TODO: Só o admin poderá ver essa rota
+complaintsRoutes.get("/:id", complaintsController.findById);
+complaintsRoutes.delete("/:id", complaintsController.delete);
 
 export { complaintsRoutes }
